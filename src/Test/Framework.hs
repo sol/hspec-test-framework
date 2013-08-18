e1eaded4aa18f0bfd5a4153d4d73f33a545a90e1
Simon Hengel <sol@typeful.net>
2013-08-18 11:53:03 +0200

    Run tests in parallel

--- src/Test/Framework.hs
+++ src/Test/Framework.hs
@@ -1,12 +1,13 @@
 module Test.Framework where
 
+import           Test.Hspec (parallel)
 import           Test.Hspec.Core (fromSpecList, describe)
 import           Test.Hspec.Runner (hspec)
 
 import           Test.Framework.Providers.API
 
 defaultMain :: [Test] -> IO ()
-defaultMain = hspec . fromSpecList
+defaultMain = hspec . parallel . fromSpecList
 
 testGroup :: TestName -> [Test] -> Test
 testGroup = describe
